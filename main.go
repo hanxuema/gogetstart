5deb561fce306fd1dabf2243fa38d05e1b154f43
Xavier Xie <xieyuehao@gmail.com>
2019-12-17 22:09:03 +1000

    upto pointer

--- main.go
+++ main.go
@@ -5,19 +5,7 @@ import (
 )
 
 func main() {
-	var i int
-	i = 42
-	fmt.Println(i)
-
-	var f float32 = 3.14
-	fmt.Println(f)
-
-	firstname := "arthur"
-	fmt.Println(firstname)
-
-	c := complex(3, 4)
-	fmt.Println(c)
-
-	r, im := real(c), imag(c)
-	fmt.Println(r,im)
+	var first *string = new(string)
+	*first = "archur" //first is the pointer
+	fmt.Println(*first) //string s
 }
