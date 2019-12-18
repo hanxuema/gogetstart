c8fe2f42f46149723e3356f5136f1fa35857231b
Xavier Xie <xieyuehao@gmail.com>
2019-12-18 21:04:15 +1000

    upto constant

--- main.go
+++ main.go
@@ -4,8 +4,20 @@ import (
 	"fmt"
 )
 
+const (
+	first  = iota //0
+	second = iota //1
+	third  = iota //2
+	fifth         //3
+)
+
+const (
+	forth = iota //0 reset
+	six          //1
+)
+
 func main() {
-	var first *string = new(string)
-	*first = "archur" //first is the pointer
-	fmt.Println(*first) //string s
+	const c int = 3
+	fmt.Println(c)
+	fmt.Println(first, second, third, fifth, forth, six)
 }
