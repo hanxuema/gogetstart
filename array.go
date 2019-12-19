f8e59a9d1549276a3cd8afea05614d12aa3f03d5
Xavier Xie <xieyuehao@gmail.com>
2019-12-19 23:06:22 +1000

    add array and slice

--- array.go
+++ array.go
@@ -5,9 +5,22 @@ import (
 )
 
 func arraymain() {
-	var arr [3]int
+	var arr [3]int 
 	fmt.Println(arr)
-
+	arr[0] = 1
+	arr[1] = 2 
+	arr[2] = 3
 	arr2 := [3]int{1, 2, 3}
 	fmt.Println(arr2)
+
+	slice := arr[:]
+	fmt.Println(slice)
+	slice = append(slice, 4,5,6)
+	fmt.Println(slice)
+	
+	s2 := slice[1:]
+	s3 := slice[:2]
+	s4 := slice[1:2]
+
+	fmt.Println(s2,s3,s4)
 }
