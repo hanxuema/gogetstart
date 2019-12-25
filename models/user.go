063c9badc31ee2f5fb789dace3f5404ebc1c9db5
Xavier Xie <xieyuehao@gmail.com>
2019-12-25 15:23:24 +1000

    add update user and remove user by id

--- models/user.go
+++ models/user.go
@@ -1,5 +1,9 @@
 package models
 
+import "errors"
+
+import "fmt"
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,8 +20,39 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("New user must has no id")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+	return User{}, fmt.Errorf("user with id '%v' not found", id)
+}
+
+func UpdateUser(u User) (User, error)  {
+	for i, candidate := range users {
+		if candidate.ID == u.ID{
+			users[i] = &u
+		}
+	}
+	return User{}, fmt.Errorf("user with id '%v' not found", u.ID)
+}
+
+func RemoveUserById(id int) error  {
+	for i, u := range users {
+		if u.ID == id {
+			users =  append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID")
 }
