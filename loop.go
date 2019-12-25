bcd8dba17fb53faa33bbc1bfbc6dcfaa64a819b8
Xavier Xie <xieyuehao@gmail.com>
2019-12-25 14:54:50 +1000

    more loop

--- loop.go
+++ loop.go
@@ -16,4 +16,13 @@ func loops() {
 		println(i)
 	}
 
+	slice := []int{1,2,3}
+	for i, v := range slice {
+		println(i, v)
+	}
+
+	ports := map[string]int{"http": 80, "https":443}
+	for k, v := range ports {
+		println(k,v)
+	}
 }
